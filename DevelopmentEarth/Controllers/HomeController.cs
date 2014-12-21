974d5f09696460d155d223b84c480bb63a6af1e3
ManishBhakuni <patexample11@gmail.com>
2014-12-21 05:46:50 +0530

    Date 21-12-2014
    This is the first commit which have all the files regarding the project.

---
+++ DevelopmentEarth/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevelopmentEarth.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        #region Home Controller Index Page
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region AboutUs
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region Services
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region Terms
+
+        public ActionResult Terms()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region ContactUs
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
---
+++ DevelopmentEarth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevelopmentEarth.MvcApplication" Language="C#" %>
